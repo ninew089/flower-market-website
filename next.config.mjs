7c1b686a6c7af72ae36bed2d4c245796fc0a9830
ninew089 <132679550+nongnew52@users.noreply.github.com>
2023-10-31 09:54:21 +0700

    feat: add docker file and stand alone

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,17 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
+await import('./src/env.mjs');
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'standalone',
   async redirects() {
     return [
       {
-        source: "/",
-        destination: "/market",
+        source: '/',
+        destination: '/market',
         permanent: true,
       },
     ];
@@ -23,11 +24,11 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
   images: {
-    domains: ["loremflickr.com", "picsum.photos"],
+    domains: ['loremflickr.com', 'picsum.photos'],
   },
 };
 export default config;
